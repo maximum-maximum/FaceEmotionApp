724db62761d603620e1fa3d9338dd2c11d5bfc8e
Koki Makishima <mr_ver74@yahoo.co.jp>
2021-09-22 17:47:51 +0900

    細かな修正

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const hiddenCanvas = document.getElementById('hidden-canvas');
 const grayScaleBtn = document.getElementById('gray-scale-btn');
 const lineDrawBtn = document.getElementById('linedraw-btn');
 const downloadBtn = document.getElementById('download-btn');
-
 const findFaceBtn = document.getElementById('findface-btn');
 
 
@@ -82,81 +81,51 @@ findFaceBtn.addEventListener('click', e => {
     const utils = new Utils('errorMessage');
     const faceCascadeFile = 'haarcascade_frontalface_default.xml'; 
     utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {
-    let src = cv.imread(srcImg);
-    let gray = new cv.Mat();
-    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
-    let faces = new cv.RectVector();
-    let faceCascade = new cv.CascadeClassifier();
-    faceCascade.load(faceCascadeFile); 
-    let msize = new cv.Size(0, 0);
-    faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize); 
-    
-    for (let i = 0; i < faces.size(); ++i) {
-        let roiGray = gray.roi(faces.get(i));
-        let roiSrc = src.roi(faces.get(i));
-        let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
-        let point2 = new cv.Point(faces.get(i).x + faces.get(i).width, faces.get(i).y + faces.get(i).height);
-        cv.rectangle(src, point1, point2, [255, 0, 0, 255]);
-        roiGray.delete(); 
-        roiSrc.delete();
-    }
-
-    cv.imshow('dest-canvas', src);
-
-    // console.log(faces);
-    var canvas = document.getElementById('dest-canvas');
-    var ctx = canvas.getContext('2d'); 
-    // ctx.fillStyle = 'rgba(255,0,255,0.2)';
-    // ctx.rect(faces.get(0).x, faces.get(0).y, faces.get(0).width, faces.get(0).height);
-    // ctx.fill();
-
-
-    var continuous = false;
-
-    canvas.addEventListener('mousemove', e => {
-        if (ctx.isPointInPath(e.offsetX, e.offsetY)) {
-            if (!continuous) {
-                ctx.fillStyle = 'rgba(255,0,255,0.2)';
-                ctx.fill();
-            }
-            continuous = true;
-            console.log('true');
-            
-        }
-        else {
-            continuous = false;
-            cv.imshow('dest-canvas', src);
-            ctx.rect(faces.get(0).x, faces.get(0).y, faces.get(0).width, faces.get(0).height);
-            console.log('false');
-        } 
-    });
-
-    // canvas.addEventListener('mousemove', e => {
-    //     // console.log(e.offsetX+':'+e.offsetY);
-    //     ctx.rect(faces.get(0).x, faces.get(0).y, faces.get(0).width, faces.get(0).height);
-    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //     if (ctx.isPointInPath(e.offsetX, e.offsetY)) {
-    //         ctx.fillStyle = 'green';
-    //         console.log('true');
-    //     }
-    //     else {
-    //         ctx.fillStyle = 'red';
-    //         console.log('false');
-    //     }
+        let src = cv.imread(srcImg);
+        let gray = new cv.Mat();
+        cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+        let faces = new cv.RectVector();
+        let faceCascade = new cv.CascadeClassifier();
+        faceCascade.load(faceCascadeFile); 
+        let msize = new cv.Size(0, 0);
+        faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize); 
         
-    //     cv.imshow('dest-canvas', src);
-    //     // ctx.rect(faces.get(0).x, faces.get(0).y, faces.get(0).width, faces.get(0).height);
-    //     ctx.fill();
-    // });
+        for (let i = 0; i < faces.size(); ++i) {
+            let roiGray = gray.roi(faces.get(i));
+            let roiSrc = src.roi(faces.get(i));
+            let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
+            let point2 = new cv.Point(faces.get(i).x + faces.get(i).width, faces.get(i).y + faces.get(i).height);
+            cv.rectangle(src, point1, point2, [255, 0, 0, 255]);
+            roiGray.delete(); 
+            roiSrc.delete();
+        }
 
-    // src.delete(); 
-    // gray.delete(); 
-    // faceCascade.delete();
-    // faces.delete(); 
+        cv.imshow('dest-canvas', src);
 
+        // console.log(faces);
+        var canvas = document.getElementById('dest-canvas');
+        var ctx = canvas.getContext('2d'); 
+        ctx.rect(faces.get(0).x, faces.get(0).y, faces.get(0).width, faces.get(0).height);
+        
+        var continuous = false;
 
-    }); 
-    
+        canvas.addEventListener('mousemove', e => {
+            if (ctx.isPointInPath(e.offsetX, e.offsetY)) {
+                console.log('true');
+                if (!continuous) {
+                    ctx.fillStyle = 'rgba(255, 0, 255, 0.2)';
+                    ctx.fill();
+                }
+                continuous = true;
+            }
+            else {
+                console.log('false');
+                continuous = false;
+                cv.imshow('dest-canvas', src);
+                ctx.rect(faces.get(0).x, faces.get(0).y, faces.get(0).width, faces.get(0).height);
+            } 
+        });
+    });   
 });
 
 function isInCheck(faces) {
@@ -167,8 +136,6 @@ function isInCheck(faces) {
     ctx.fill();
 
     canvas.addEventListener('mousemove', e => {
-        console.log(e.offsetX+':'+e.offsetY);
-
         if (ctx.isPointInPath(e.offsetX, e.offsetY)) {
             ctx.fillStyle = 'green';
             console.log('true');
@@ -177,8 +144,6 @@ function isInCheck(faces) {
             ctx.fillStyle = 'red';
             console.log('false');
         }
-
-        // Draw circle
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.fill();
     });
