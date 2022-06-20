12ed74259032fdff187d4141ca676446e81e168a
Koki Makishima <mr_ver74@yahoo.co.jp>
2022-06-20 23:06:13 +0900

    Updare Detection

--- front/src/App.js
+++ front/src/App.js
@@ -1,3 +1,4 @@
+import Lena from "./lena.jpg";
 import { ReactComponent as Github } from "./github.svg";
 import "./App.css";
 
@@ -5,8 +6,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Detection />
-      <Prediction />
+      <Container />
       <Footer />
     </div>
   );
@@ -14,25 +14,61 @@ function App() {
 
 function Header() {
   return (
-    <div>
-      <header>
-        <h1>Face Emotion App</h1>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/maximum-maximum/FaceEmotionApp"
-        >
-          <Github />
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1>Face Emotion App</h1>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/maximum-maximum/FaceEmotionApp"
+      >
+        <Github />
+      </a>
+    </header>
   );
 }
 
+function Container() {
+  return (
+    <div className="container">
+      <Detection />
+      <Prediction />
+    </div>
+  );
+}
 function Detection() {
   return (
-    <div>
-      <p>hoge</p>
+    <div className="face-detection">
+      <h3>Face Detection</h3>
+      <div className="upper-content">
+        <div className="source">
+          <a id="source">Source</a>
+          <img id="src-image" src={Lena} />
+          <input type="file" id="input-file" />
+        </div>
+        <div className="output">
+          <a id="output"></a>
+          <canvas id="dest-canvas"></canvas>
+          <br />
+          <input
+            type="button"
+            className="button"
+            value="顔検出"
+            id="detectface-btn"
+          />
+          <input
+            type="button"
+            className="button"
+            value="予測"
+            id="predict-btn"
+          />
+          <input
+            type="button"
+            className="button"
+            value="リセット"
+            onclick="window.location.reload();"
+          />
+        </div>
+      </div>
     </div>
   );
 }
