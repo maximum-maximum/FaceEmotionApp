69881eb15be9b82539635017a366488cc9616b96
Koki Makishima <mr_ver74@yahoo.co.jp>
2022-06-20 23:20:55 +0900

    Update prediction

--- front/src/App.js
+++ front/src/App.js
@@ -35,6 +35,7 @@ function Container() {
     </div>
   );
 }
+
 function Detection() {
   return (
     <div className="face-detection">
@@ -76,7 +77,50 @@ function Detection() {
 function Prediction() {
   return (
     <div>
-      <p>hoge</p>
+      <h3>Emotion Prediction</h3>
+      <div className="bottom-content">
+        <div className="selected-image">
+          <a id="selected-image"></a>
+          <canvas id="face-canvas"></canvas>
+        </div>
+        <div className="res">
+          <a id="res"></a>
+          <table className="result-table" id="result-table">
+            <tr>
+              <th id="emotion"></th>
+              <th id="accuracy"></th>
+            </tr>
+            <tr className="result">
+              <td className="emotion"></td>
+              <td className="accuracy"></td>
+            </tr>
+            <tr className="result">
+              <td className="emotion"></td>
+              <td className="accuracy"></td>
+            </tr>
+            <tr className="result">
+              <td className="emotion"></td>
+              <td className="accuracy"></td>
+            </tr>
+            <tr className="result">
+              <td className="emotion"></td>
+              <td className="accuracy"></td>
+            </tr>
+            <tr className="result">
+              <td className="emotion"></td>
+              <td className="accuracy"></td>
+            </tr>
+            <tr className="result">
+              <td className="emotion"></td>
+              <td className="accuracy"></td>
+            </tr>
+            <tr className="result">
+              <td className="emotion"></td>
+              <td className="accuracy"></td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
