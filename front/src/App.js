38516143a31456d3deb97079668ad4fb3102dc56
Koki Makishima <mr_ver74@yahoo.co.jp>
2022-06-21 17:55:38 +0900

    component化

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
-import Lena from "./lena.jpg";
-import { ReactComponent as Github } from "./github.svg";
 import "./App.css";
+import Header from "./components/Header";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,127 +9,6 @@ function App() {
       <Header />
       <Container />
       <Footer />
-    </div>
-  );
-}
-
-function Header() {
-  return (
-    <header>
-      <h1>Face Emotion App</h1>
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://github.com/maximum-maximum/FaceEmotionApp"
-      >
-        <Github />
-      </a>
-    </header>
-  );
-}
-
-function Container() {
-  return (
-    <div className="container">
-      <Detection />
-      <Prediction />
-    </div>
-  );
-}
-
-function Detection() {
-  return (
-    <div className="face-detection">
-      <h3>Face Detection</h3>
-      <div className="upper-content">
-        <div className="source">
-          <a id="source">Source</a>
-          <img id="src-image" src={Lena} alt="lena" />
-          <input type="file" id="input-file" />
-        </div>
-        <div className="output">
-          <a id="output"></a>
-          <canvas id="dest-canvas"></canvas>
-          <br />
-          <input
-            type="button"
-            className="button"
-            value="顔検出"
-            id="detectface-btn"
-          />
-          <input
-            type="button"
-            className="button"
-            value="予測"
-            id="predict-btn"
-          />
-          <input
-            type="button"
-            className="button"
-            value="リセット"
-            onclick="window.location.reload();"
-          />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Prediction() {
-  return (
-    <div>
-      <h3>Emotion Prediction</h3>
-      <div className="bottom-content">
-        <div className="selected-image">
-          <a id="selected-image"></a>
-          <canvas id="face-canvas"></canvas>
-        </div>
-        <div className="res">
-          <a id="res"></a>
-          <table className="result-table" id="result-table">
-            <tr>
-              <th id="emotion"></th>
-              <th id="accuracy"></th>
-            </tr>
-            <tr className="result">
-              <td className="emotion"></td>
-              <td className="accuracy"></td>
-            </tr>
-            <tr className="result">
-              <td className="emotion"></td>
-              <td className="accuracy"></td>
-            </tr>
-            <tr className="result">
-              <td className="emotion"></td>
-              <td className="accuracy"></td>
-            </tr>
-            <tr className="result">
-              <td className="emotion"></td>
-              <td className="accuracy"></td>
-            </tr>
-            <tr className="result">
-              <td className="emotion"></td>
-              <td className="accuracy"></td>
-            </tr>
-            <tr className="result">
-              <td className="emotion"></td>
-              <td className="accuracy"></td>
-            </tr>
-            <tr className="result">
-              <td className="emotion"></td>
-              <td className="accuracy"></td>
-            </tr>
-          </table>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <div>
-      <p>hoge</p>
     </div>
   );
 }
